c3c7c59c7fc15e3ed24ad06e186f5e262662c9fe
yangfengling <yangfengling@meituan.com>
2018-11-20 09:00:03 +0800

    5-14

--- src/pages/game-page.js
+++ src/pages/game-page.js
@@ -1,4 +1,7 @@
 import { scene } from '../scene/index'
+import Cuboid from '../block/cuboid'
+import Cylinder from '../block/cylinder'
+
 export default class GamePage {
     constructor (callbacks) {
         this.callbacks = callbacks;
@@ -7,12 +10,19 @@ export default class GamePage {
     init () {
         this.scene = scene;
         this.scene.init();
+        this.addInitBlock();
         this.render();
     }
 
     render () {
         this.scene.render();
         requestAnimationFrame(this.render.bind(this))
+    }
+    addInitBlock() {
+        const cuboidBlock = new Cuboid(-15, 0, 0);
+        const cylinderBlock = new Cylinder(23, 0, 0);
+        this.scene.instance.add(cuboidBlock.instance);
+        this.scene.instance.add(cylinderBlock.instance);
     }
 }
 
